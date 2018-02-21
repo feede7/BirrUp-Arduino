d4d0b13904abba9fd3b54424bb139873ee143eb4
Fede <feede.7@gmail.com>
2018-02-20 21:33:00 -0300

    Andando RFID con LED indicador de tarjeta y conectado tal cual está en la página que puse en el README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BirrUp-Arduino
 
 Arduino NANO
+Librería RFID --> https://github.com/miguelbalboa/rfid
+Tuto RFID --> http://www.naylampmechatronics.com/blog/22_Tutorial-Lector-RFID-RC522.html
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,11 +1,37 @@
+#include <MFRC522.h>
+#include <SPI.h>
+
+#define RST_PIN  9    //Pin 9 para el reset del RC522
+#define SS_PIN  10   //Pin 10 para el SS (SDA) del RC522
+#define LED_PIN 14
+MFRC522 mfrc522(SS_PIN, RST_PIN); //Creamos el objeto para el RC522
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); // Iniciamos la comunicación  serial
+  SPI.begin();          // Iniciamos el Bus SPI
+  mfrc522.PCD_Init();   // Iniciamos  el MFRC522
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN,HIGH);
+  Serial.println("Lectura del UID");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(millis()%500 == 0)
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
-  delay(1);
+  if (mfrc522.PICC_IsNewCardPresent()) {
+    //Seleccionamos una tarjeta
+    digitalWrite(LED_PIN,LOW);
+    if (mfrc522.PICC_ReadCardSerial()) {
+      // Enviamos serialemente su UID
+      Serial.print("Card UID:");
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(mfrc522.uid.uidByte[i], HEX);   
+      } 
+      Serial.println();
+      // Terminamos la lectura de la tarjeta  actual
+      mfrc522.PICC_HaltA();         
+    }      
+  } else digitalWrite(LED_PIN,HIGH);
 }
